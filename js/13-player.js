183773e1f2c9d157c64610f7bfb05b5da1775634
Jhonatan Vinicius <jhonnytrabalho2005@gmail.com>
2023-11-25 09:29:06 -0300

    finalizado

--- js/13-player.js
+++ js/13-player.js
@@ -1,10 +1,24 @@
 'use strict';
-
+// Selecionando <select>
 const lista = document.querySelector("#lista-de-musicas");
+
+// Selecionando o <audio>
 const reprodutor = document.querySelector("audio");
+
+// Selecionando <figure> e seus elementos
+const figureCapa = document.querySelector("figure");
+
+const imagemCapa = figureCapa.querySelector("img");
+const tituloMusica = figureCapa.querySelector("figcaption");
 
 // Monitorando o evento de mudanças de opções na lista <select>
 lista.addEventListener("change", function(){
+
+    /* Verificando se o figure está escondido */
+    if(figureCapa.hasAttribute("hidden")){
+        figureCapa.removeAttribute("hidden");
+    }
+
     /* Selecionando o <option> que foi escolhido
     dentro da lista <select> */
     let musicaSelecionada = lista.options[lista.selectedIndex];
@@ -12,9 +26,16 @@ lista.addEventListener("change", function(){
 
     reprodutor.src = musicaSelecionada.value;
 
+    // Exibindo o titulo da música
+    tituloMusica.textContent = musicaSelecionada.textContent;
+
+    // Exibindo a capa no img
+    imagemCapa.src = musicaSelecionada.getAttribute("data-imagem")
+
     /* Reproduzindo a música somente se o valor de musicaSelecionada for 
     diferente de vazio. Usamos o operador !== (diferente) */
     if(musicaSelecionada.value !== ""){
         reprodutor.play();
     }
 });
+
